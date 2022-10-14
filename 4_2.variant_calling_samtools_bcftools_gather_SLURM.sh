90f2f7a259a848867b9a4247292359af7f1c3923
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2022-10-14 12:58:53 +0200

    Update 4_2.variant_calling_samtools_bcftools_gather_SLURM.sh

--- 4_2.variant_calling_samtools_bcftools_gather_SLURM.sh
+++ 4_2.variant_calling_samtools_bcftools_gather_SLURM.sh
@@ -3,18 +3,17 @@
 #SBATCH --time=10:00:00
 #SBATCH --mem=60gb
 
-PIC=/home/users/katarzynaburda/tools/picard/picard.jar
-DIR=/home/users/katarzynaburda/MUTATION_RATE/SNP_CALLING/SAMTOOLS/final_T10-O
+PIC=dir/to/picard.jar
+DIR=dir/to/raw_vcfs
 
 module load java8
 module load bcftools/1.9
 
-## What to combine?
-t=$1
-
 ## Combine
 java -Xmx60g -jar ${PIC} GatherVcfs \
-TMP_DIR=/home/users/katarzynaburda/tmp \
-R=/home/users/katarzynaburda/MUTATION_RATE/REF_GENOME/guppy.fa \
-O=${DIR}/${t}/ALL.vcf.gz \
-I=${DIR}/${t}/vcf_list.txt
+TMP_DIR=dir/to/temporary_files \
+R=dir/to/ref/guppy.fa \
+O=${DIR}/ALL.vcf.gz \
+I=${DIR}/vcf_list.txt
+
+# ^ It's a list of vcf files from all scaffolds with combined individuals.
