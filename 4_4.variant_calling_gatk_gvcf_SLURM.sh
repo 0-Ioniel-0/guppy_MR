d4de274b7d8b8b0195e648572d72a6de8f86b835
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2022-12-02 16:07:39 +0100

    Update 4_4.variant_calling_gatk_gvcf_SLURM.sh

--- 4_4.variant_calling_gatk_gvcf_SLURM.sh
+++ 4_4.variant_calling_gatk_gvcf_SLURM.sh
@@ -7,6 +7,7 @@
 
 module load java8
 
+# It's best to use newest version of GATK(4.3)
 GA=dir/to/gatk
 IN=dir/to/ready_bams/MAPPED
 OU=dir/to/vcf/GATK
@@ -19,7 +20,7 @@ ${GA} --java-options "-Xmx60g" HaplotypeCaller \
 -R ${RE} \
 -I ${IN}/${i}_ready.bam \
 -O ${OU}/${i}.g.vcf.gz \
--ERC GVCF \
+-ERC BP_RESOLUTION \
 --pcr-indel-model NONE \
 --output-mode EMIT_ALL_CONFIDENT_SITES \
 --native-pair-hmm-threads 20
