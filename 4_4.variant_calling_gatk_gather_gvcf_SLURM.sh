cf9167983572f49783d69a006cd26019772f1324
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2022-12-02 16:09:34 +0100

    Update and rename 4_3.variant_calling_gatk_gather_gvcf_SLURM.sh to 4_4.variant_calling_gatk_gather_gvcf_SLURM.sh

--- 4_4.variant_calling_gatk_gather_gvcf_SLURM.sh
+++ 4_4.variant_calling_gatk_gather_gvcf_SLURM.sh
@@ -24,6 +24,11 @@ ${GATK} CombineGVCFs \
 --variant ${DIR}/T7-M-1.g.vcf.gz \
 --variant ${DIR}/T7-M-2.g.vcf.gz \
 --variant ${DIR}/T7-O.g.vcf.gz \
+-O T7_family_final.g.vcf.gz
+
+
+${GATK} CombineGVCFs \
+-R ${REF} \
 --variant ${DIR}/T10-A.g.vcf.gz \
 --variant ${DIR}/T10-F-1.g.vcf.gz \
 --variant ${DIR}/T10-F-2.g.vcf.gz \
@@ -35,8 +40,8 @@ ${GATK} CombineGVCFs \
 --variant ${DIR}/T10-J-6.g.vcf.gz \
 --variant ${DIR}/T10-M-1.g.vcf.gz \
 --variant ${DIR}/T10-M-2.g.vcf.gz \
---variant ${DIR}/T10-O_final.g.vcf.gz \
--O ALL_final.g.vcf.gz
+--variant ${DIR}/T10-O.g.vcf.gz \
+-O T10_family_final.g.vcf.gz
 
 echo 'COMBINING DONE'
 date
