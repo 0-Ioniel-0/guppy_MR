932b15e7a5f412867a2a11d360f8ae8d6872ac5e
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2022-12-02 12:46:36 +0100

    Update 4_5.variant_calling_gatk_call_SLURM.sh

--- 4_5.variant_calling_gatk_call_SLURM.sh
+++ 4_5.variant_calling_gatk_call_SLURM.sh
@@ -2,11 +2,7 @@
 
 #SBATCH --mem=60g
 
-D=/home/users/katarzynaburda/MUTATION_RATE/SNP_CALLING/GATK
-R=/home/users/katarzynaburda/MUTATION_RATE/REF_GENOME/guppy.fna
-G=/home/users/katarzynaburda/tools/gatk-4.1.4.1/gatk
-
 module load java8
 
-${G} --java-options "-Xmx60g" GenotypeGVCFs \
--R ${R} -V ${D}/ALL_final.g.vcf.gz -O ${D}/final/ALL_final.vcf.gz
+gatk --java-options "-Xmx60g" GenotypeGVCFs \
+-R reference.fa -V merged.g.vcf.gz -O final.vcf.gz
