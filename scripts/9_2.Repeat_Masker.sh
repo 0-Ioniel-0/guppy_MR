c4ddfeffce9784536447e275cc977f530ca1d7d6
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2023-05-19 11:27:47 +0200

    Update 9_2.Repeat_Masker.sh

--- scripts/9_2.Repeat_Masker.sh
+++ scripts/9_2.Repeat_Masker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ################# Masking Repeats #################
-# 
+# Note that we used Danio rerio repeats, because guppy specific repeats were not available.
+RepeatMasker -pa 20  -gff -species danio genome.fna
